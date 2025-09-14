1deb89a4bfe959f2ea2cf027e71f56cc349d2555
diego.salazar01 <diego.salazar01@globant.com>
2025-09-13 20:49:34 -0500

    Add test for successful login flow

--- src/test/java/com/automation/screens/LoginScreen.java
+++ src/test/java/com/automation/screens/LoginScreen.java
@@ -1,11 +1,13 @@
 package com.automation.screens;
 
 import com.automation.utils.BaseScreen;
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginScreen extends BaseScreen {
 
@@ -14,6 +16,21 @@ public class LoginScreen extends BaseScreen {
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Sign up\")")
     WebElement btnSignUp;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Email\")")
+    WebElement txtLoginEmail;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Password\")")
+    WebElement txtLoginPassword;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.view.ViewGroup\").instance(16)")
+    WebElement btnLogin;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"android:id/alertTitle\")")
+    WebElement lblLoginAlertTitle;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"android:id/message\")")
+    WebElement lblLoginAlertMessage;
 
     public LoginScreen(AppiumDriver driver) {
         super(driver);
@@ -24,8 +41,27 @@ public class LoginScreen extends BaseScreen {
     }
 
     public SignUpScreen tapOnSignUpButton() {
-        btnSignUp.click();
+        click(btnSignUp);
         return new SignUpScreen(driver);
+    }
+
+    public void enterLoginEmail(String email) {
+        sendKeys(txtLoginEmail, email);
+    }
+
+    public void enterLoginPassword(String password) {
+        sendKeys(txtLoginPassword, password);
+    }
+
+    public void tapOnLoginButton() {
+        click(btnLogin);
+    }
+
+    public boolean isLoggedInMessageDisplayed() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        WebElement element = wait.until(ExpectedConditions.visibilityOf(lblLoginAlertTitle));
+        WebElement element1 = wait.until(ExpectedConditions.visibilityOf(lblLoginAlertMessage));
+        return true;
     }
 
 }
